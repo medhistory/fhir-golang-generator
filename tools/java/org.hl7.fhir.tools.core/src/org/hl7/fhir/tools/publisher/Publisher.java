cae761e3d0cb08555ad02efebf0d6be7dee671f6
Chris Moesel <cmoesel@mitre.org>
2015-06-17 11:46:53 -0400

    Move @ahubley's DSTU1 based go generator from internal repo to DSTU2 fork on Github.  Straight copy, so code doesn't (yet) compile w/ DSTU2.

--- tools/java/org.hl7.fhir.tools.core/src/org/hl7/fhir/tools/publisher/Publisher.java
+++ tools/java/org.hl7.fhir.tools.core/src/org/hl7/fhir/tools/publisher/Publisher.java
@@ -206,6 +206,7 @@ import org.hl7.fhir.tools.converters.ValueSetImporterV3;
 import org.hl7.fhir.tools.implementations.XMLToolsGenerator;
 import org.hl7.fhir.tools.implementations.csharp.CSharpGenerator;
 import org.hl7.fhir.tools.implementations.delphi.DelphiGenerator;
+import org.hl7.fhir.tools.implementations.go.GoGenerator;
 import org.hl7.fhir.tools.implementations.java.JavaGenerator;
 import org.hl7.fhir.tools.implementations.javascript.JavaScriptGenerator;
 import org.hl7.fhir.tools.publisher.ExampleInspector.EValidationFailed;
@@ -1518,6 +1519,7 @@ public class Publisher implements URIResolver, SectionNumberer {
     page.getReferenceImplementations().add(new XMLToolsGenerator());
     page.getReferenceImplementations().add(new JavaScriptGenerator());
 //    page.getReferenceImplementations().add(new EMFGenerator());
+    page.getReferenceImplementations().add(new GoGenerator());
 
     // page.getReferenceImplementations().add(new ECoreOclGenerator());
   }
@@ -2684,7 +2686,7 @@ public class Publisher implements URIResolver, SectionNumberer {
           checkElement(sd, ed, false);
       }
     }
-    
+
   }
 
   private void checkElement(StructureDefinition sd, ElementDefinition ed, boolean inDiff) {
@@ -2752,7 +2754,7 @@ public class Publisher implements URIResolver, SectionNumberer {
     case LOGICAL: return checkLogical(sd);
     default:
       check(false, sd, "Unknown kind");
-      return false;    
+      return false;
     }
   }
 
@@ -2786,7 +2788,7 @@ public class Publisher implements URIResolver, SectionNumberer {
 
   private void check(boolean pass, StructureDefinition sd, String msg) {
     if (!pass)
-      System.out.println("Error in StructureDefinition "+sd.getId()+": "+msg);    
+      System.out.println("Error in StructureDefinition "+sd.getId()+": "+msg);
   }
 
   private String tail(String url) {
